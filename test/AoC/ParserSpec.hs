919de7358de6fd784c0216a35f041be20d605911
James Santucci <james.santucci@gmail.com>
2023-12-01 21:27:15 -0800

    Test fromMap parser constructor

---
+++ test/AoC/ParserSpec.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module AoC.ParserSpec where
+
+import qualified AoC.Parser as Parser
+import Data.Either (isLeft)
+import qualified Data.Map as Map
+import Test.Hspec (Spec, describe, it, shouldBe)
+import Text.Megaparsec (runParser)
+
+spec :: Spec
+spec = describe "Parser" $ do
+  describe "fromMap" $ do
+    it "should build a parser that fails for any input from an empty map" $
+      isLeft (runParser (Parser.fromMap mempty) "" "abcde") `shouldBe` True
+    it "should build a parser that succeeds on the correct input for a single-element map" $
+      let parserMap :: Map.Map String Int
+          parserMap = Map.fromList [("abc", 1)]
+       in runParser (Parser.fromMap parserMap) "" "abc" `shouldBe` Right 1
+    it "should build a parser that succeeds on the correct inputs for a two-element map" $
+      let parserMap :: Map.Map String Int
+          parserMap = Map.fromList [("abc", 1), ("def", 2)]
+       in do
+            runParser (Parser.fromMap parserMap) "" "abc" `shouldBe` Right 1
+            runParser (Parser.fromMap parserMap) "" "def" `shouldBe` Right 2
+            isLeft (runParser (Parser.fromMap parserMap) "" "bogus") `shouldBe` True
