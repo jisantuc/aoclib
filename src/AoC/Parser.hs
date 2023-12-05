ca126095cd3be3298f87f1d27965497fefbd0581
James Santucci <james.santucci@gmail.com>
2023-12-04 20:09:26 -0800

    Convert parser helpers from bytestring to text

--- src/AoC/Parser.hs
+++ src/AoC/Parser.hs
@@ -1,20 +1,19 @@
 module AoC.Parser where
 
-import Data.ByteString (ByteString)
-import qualified Data.ByteString.Char8 as BSChar
 import Data.Functor (($>))
 import qualified Data.Map as Map
+import Data.Text (Text)
 import Data.Void (Void)
 import Text.Megaparsec (ParseErrorBundle, Parsec, runParser, (<|>))
 import Text.Megaparsec.Byte (string)
 
-type Parser = Parsec Void ByteString
+type Parser = Parsec Void Text
 
-fromMap :: Map.Map String v -> Parser v
+fromMap :: Map.Map Text v -> Parser v
 fromMap =
   Map.foldlWithKey'
     ( \parser k parsed ->
-        parser <|> string (BSChar.pack k) $> parsed
+        parser <|> string k $> parsed
     )
     (fail "empty map")
 
--- test/AoC/ParserSpec.hs
+++ test/AoC/ParserSpec.hs
@@ -5,6 +5,7 @@ module AoC.ParserSpec where
 import qualified AoC.Parser as Parser
 import Data.Either (isLeft)
 import qualified Data.Map as Map
+import Data.Text (Text)
 import Test.Hspec (Spec, describe, it, shouldBe)
 import Text.Megaparsec (runParser)
 
@@ -14,11 +15,11 @@ spec = describe "Parser" $ do
     it "should build a parser that fails for any input from an empty map" $
       isLeft (runParser (Parser.fromMap mempty) "" "abcde") `shouldBe` True
     it "should build a parser that succeeds on the correct input for a single-element map" $
-      let parserMap :: Map.Map String Int
+      let parserMap :: Map.Map Text Int
           parserMap = Map.fromList [("abc", 1)]
        in runParser (Parser.fromMap parserMap) "" "abc" `shouldBe` Right 1
     it "should build a parser that succeeds on the correct inputs for a two-element map" $
-      let parserMap :: Map.Map String Int
+      let parserMap :: Map.Map Text Int
           parserMap = Map.fromList [("abc", 1), ("def", 2)]
        in do
             runParser (Parser.fromMap parserMap) "" "abc" `shouldBe` Right 1
