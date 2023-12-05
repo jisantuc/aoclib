44aa84f18b92b839a8e9b7fe3a1897318be10a2f
James Santucci <james.santucci@gmail.com>
2023-12-04 20:14:24 -0800

    Update testing helpers to use text instead of bytestring

--- src/AoC/Parser/Testing.hs
+++ src/AoC/Parser/Testing.hs
@@ -6,20 +6,20 @@ module AoC.Parser.Testing
 where
 
 import Control.Monad (when)
-import Data.ByteString (ByteString)
+import Data.Text (Text)
 import Data.Void (Void)
 import Test.Hspec (Expectation)
 import Text.Megaparsec (ParseErrorBundle, errorBundlePretty)
 
-expectSuccessfulParse :: (Show a) => Either (ParseErrorBundle ByteString Void) a -> Bool -> Expectation
+expectSuccessfulParse :: (Show a) => Either (ParseErrorBundle Text Void) a -> Bool -> Expectation
 expectSuccessfulParse (Left err) _ = fail . errorBundlePretty $ err
 expectSuccessfulParse (Right parsed) debug = when debug $ print $ show parsed
 
-expectParsed :: Either (ParseErrorBundle ByteString Void) t -> (t -> Expectation) -> Expectation
+expectParsed :: Either (ParseErrorBundle Text Void) t -> (t -> Expectation) -> Expectation
 expectParsed parseResult f = case parseResult of
   Right puzz -> f puzz
   Left e -> fail . errorBundlePretty $ e
 
-expectParsedIO :: IO (Either (ParseErrorBundle ByteString Void) t) -> (t -> Expectation) -> Expectation
+expectParsedIO :: IO (Either (ParseErrorBundle Text Void) t) -> (t -> Expectation) -> Expectation
 expectParsedIO mParseResult f =
   mParseResult >>= (`expectParsed` f)
